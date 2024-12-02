6692b28bfd8abf42cb381b8fd94d1ec9e29cefe9
Marco <85710195+Marco305278@users.noreply.github.com>
2024-12-02 16:35:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1625,7 +1625,7 @@ async function generatePreviews() {
                     } else if (graphicName === 'insta') {
                         // *** Nuova Gestione per 'insta' ***
                         // Disegna un rettangolo trasparente di 500x300px al centro del canvas
-                        const rectWidth = 943,5;
+                        const rectWidth = 943.5;
                         const rectHeight = 1182;
                         const rectX = (canvas.width - rectWidth) / 2;
                         const rectY = (canvas.height - rectHeight) / 2;
